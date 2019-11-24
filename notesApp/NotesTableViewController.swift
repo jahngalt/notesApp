f21e40a5d374e760f513cb7010d751b323b9007a
Oleg Kudimov <ovkudimov@gmail.com>
2019-11-24 19:49:07 +0300

    added delete functionality and label when table is empty

--- notesApp/NotesTableViewController.swift
+++ notesApp/NotesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NotesTableViewController: UITableViewController {
+class NotesTableViewController: UITableViewController, UITabBarDelegate {
 
     @IBAction func composePressed(_ sender: UIBarButtonItem) {
         addNote()
@@ -29,10 +29,21 @@ class NotesTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-
+    
+    // set number of rows and set label when table is empty 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return notes.count
+        if self.notes.count == 0 {
+            let emptyLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height))
+            emptyLabel.text = "No Data"
+            emptyLabel.textAlignment = NSTextAlignment.center
+            self.tableView.backgroundView = emptyLabel
+            self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+                return 0
+        } else {
+            self.tableView.backgroundView = nil
+            return self.notes.count
+        }
+    
     }
 
     
@@ -43,6 +54,29 @@ class NotesTableViewController: UITableViewController {
 
         return cell
     }
+    
+    // can we edit all row? Yes!
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // if we can edit rows, this function will execute
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteNoteAt(index: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // add footer to our table
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10.0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
     
 
     func addNote() {
@@ -55,5 +89,7 @@ class NotesTableViewController: UITableViewController {
         notes.remove(at: index)
         
     }
+    
+    
 
 }
